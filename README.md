6c294babe2def9906cbbb1975998a66f22fed105
tobias-goertz <tobias-goertz@users.noreply.github.com>
2020-06-13 16:18:32 +0000

    deploy: 73e0a6bd29501551608f4b0be9e56fe516541598

---
+++ README.md
@@ -0,0 +1,35 @@
+# manoamano-honduras
+Website of the manoamano coffee project from students in Muenster
+
+### How to get access to editing the website
+- Einen GitHub-Account erstellen
+- Von Tobi eingeladen werden am Code mitzuschreiben
+- Atom [hier](https://atom.io/) als Editor herunterladen (und sich da bei GitHub anmelden)
+- Git [hier](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) runterladen und installieren
+- Das gesamte Projekt "runterladen". Dazu "clone" das Projekt mit `git clone https://github.com/tobias-goertz/manoamano-honduras.git`, wie das geht [siehe hier](https://git-scm.com/book/de/v1/Git-Grundlagen-Ein-Git-Repository-anlegen#Ein-existierendes-Repository-klonen)
+
+### Von Bäumen und Zweigen
+Es gibt immer mindestens zwei Versionen (branches) des Codes, die zusammen den Verlauf (tree) bilden:
+- `gh-pages`: Der ist aktuell online geschaltet und wird nur von Tobi angefasst (gehostet von GitHub selbst)
+- `master`: Sollte genauso aussehen, sozusagen ein Sicherheitsschritt vor dem Hochladen
+
+### How to edit
+- Das Projekt in Atom öffnen
+- IMMER zuerst (und auch zwischendurch) unten rechts mit `fetch` neue Änderungen abrufen und mit `pull` herunterladen
+- Einen eigenen Branch `branch_name` erstellen, ausgehend vom master
+- Darin herumeditieren (alle Änderungen werden nachverfolgt) und SPEICHERN
+- Fertige Änderungen per `stage (all)` annehmen, eine Beschreibung hinzufügen
+- immer wieder `fetch` drücken (falls jemand in der Zwischenzeit dasselbe geändert hat)
+- Per `commit to (branch_name)` in die lokale auf dem Rechner gespeicherte Version hochladen
+- Per `publish` oder `push` in GitHub hochladen
+
+### Make someone review and comment on what you created
+- Den eigenen branch  in GitHub öffnen
+- `New Pull Request`, funktioniert wie ein post, man kann Leute markieren, im Code herumkommentieren und so weiter
+- Man kann markieren, ob das Projekt fertig ist oder in Arbeit und und und, aber das geht für diese Zwecke zu weit
+
+### Get your creation online
+- Tobi schreiben, er `merged` den eigenen branch mit dem master und dem gh-pages branch und stellt die Änderungen so online.
+
+### Questions?
+Schreibe eine Mail an tobias.goertz@weitblicker.org
---
+++ assets/js/main.js
@@ -0,0 +1,315 @@
+/*
+	Paradigm Shift by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+		$main = $('#wrapper');
+
+	// Breakpoints.
+		breakpoints({
+			default:   ['1681px',   null       ],
+			xlarge:    ['1281px',   '1680px'   ],
+			large:     ['981px',    '1280px'   ],
+			medium:    ['737px',    '980px'    ],
+			small:     ['481px',    '736px'    ],
+			xsmall:    ['361px',    '480px'    ],
+			xxsmall:   [null,       '360px'    ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Hack: Enable IE workarounds.
+		if (browser.name == 'ie')
+			$body.addClass('is-ie');
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+
+	// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				offset: 100
+			});
+
+	// Polyfill: Object fit.
+		if (!browser.canUse('object-fit')) {
+
+			$('.image[data-position]').each(function() {
+
+				var $this = $(this),
+					$img = $this.children('img');
+
+				// Apply img as background.
+					$this
+						.css('background-image', 'url("' + $img.attr('src') + '")')
+						.css('background-position', $this.data('position'))
+						.css('background-size', 'cover')
+						.css('background-repeat', 'no-repeat');
+
+				// Hide img.
+					$img
+						.css('opacity', '0');
+
+			});
+
+			$('.gallery > a').each(function() {
+
+				var $this = $(this),
+					$img = $this.children('img');
+
+				// Apply img as background.
+					$this
+						.css('background-image', 'url("' + $img.attr('src') + '")')
+						.css('background-position', 'center')
+						.css('background-size', 'cover')
+						.css('background-repeat', 'no-repeat');
+
+				// Hide img.
+					$img
+						.css('opacity', '0');
+
+			});
+
+		}
+
+	// Gallery.
+		$('.gallery')
+			.on('click', 'a', function(event) {
+
+				var $a = $(this),
+					$gallery = $a.parents('.gallery'),
+					$modal = $gallery.children('.modal'),
+					$modalImg = $modal.find('img'),
+					href = $a.attr('href');
+
+				// Not an image? Bail.
+					if (!href.match(/\.(jpg|gif|png|mp4)$/))
+						return;
+
+				// Prevent default.
+					event.preventDefault();
+					event.stopPropagation();
+
+				// Locked? Bail.
+					if ($modal[0]._locked)
+						return;
+
+				// Lock.
+					$modal[0]._locked = true;
+
+				// Set src.
+					$modalImg.attr('src', href);
+
+				// Set visible.
+					$modal.addClass('visible');
+
+				// Focus.
+					$modal.focus();
+
+				// Delay.
+					setTimeout(function() {
+
+						// Unlock.
+							$modal[0]._locked = false;
+
+					}, 600);
+
+			})
+			.on('click', '.modal', function(event) {
+
+				var $modal = $(this),
+					$modalImg = $modal.find('img');
+
+				// Locked? Bail.
+					if ($modal[0]._locked)
+						return;
+
+				// Already hidden? Bail.
+					if (!$modal.hasClass('visible'))
+						return;
+
+				// Stop propagation.
+					event.stopPropagation();
+
+				// Lock.
+					$modal[0]._locked = true;
+
+				// Clear visible, loaded.
+					$modal
+						.removeClass('loaded')
+
+				// Delay.
+					setTimeout(function() {
+
+						$modal
+							.removeClass('visible')
+
+						setTimeout(function() {
+
+							// Clear src.
+								$modalImg.attr('src', '');
+
+							// Unlock.
+								$modal[0]._locked = false;
+
+							// Focus.
+								$body.focus();
+
+						}, 475);
+
+					}, 125);
+
+			})
+			.on('keypress', '.modal', function(event) {
+
+				var $modal = $(this);
+
+				// Escape? Hide modal.
+					if (event.keyCode == 27)
+						$modal.trigger('click');
+
+			})
+			.on('mouseup mousedown mousemove', '.modal', function(event) {
+
+				// Stop propagation.
+					event.stopPropagation();
+
+			})
+			.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
+				.find('img')
+					.on('load', function(event) {
+
+						var $modalImg = $(this),
+							$modal = $modalImg.parents('.modal');
+
+						setTimeout(function() {
+
+							// No longer visible? Bail.
+								if (!$modal.hasClass('visible'))
+									return;
+
+							// Set loaded.
+								$modal.addClass('loaded');
+
+						}, 275);
+
+					});
+
+	// Nav.
+		var $nav = $('#nav');
+
+		if ($nav.length > 0) {
+
+			// Shrink effect.
+				$main
+					.scrollex({
+						mode: 'top',
+						enter: function() {
+							$nav.addClass('alt');
+						},
+						leave: function() {
+							$nav.removeClass('alt');
+						},
+					});
+
+			// Links.
+				var $nav_a = $nav.find('a');
+
+				$nav_a
+					.scrolly({
+						speed: 1000,
+						offset: function() { return $nav.height() - 55; }
+					})
+					.on('click', function() {
+
+						var $this = $(this);
+
+						// External link? Bail.
+							if ($this.attr('href').charAt(0) != '#')
+								return;
+
+						// Deactivate all links.
+							$nav_a
+								.removeClass('active')
+								.removeClass('active-locked');
+
+						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+							$this
+								.addClass('active')
+								.addClass('active-locked');
+
+					})
+					.each(function() {
+
+						var	$this = $(this),
+							id = $this.attr('href'),
+							$section = $(id);
+
+						// No section for this link? Bail.
+							if ($section.length < 1)
+								return;
+
+						// Scrollex.
+							$section.scrollex({
+								mode: 'middle',
+								initialize: function() {
+
+									// Deactivate section.
+										if (browser.canUse('transition'))
+											$section.addClass('inactive');
+
+								},
+								enter: function() {
+
+									// Activate section.
+										$section.removeClass('inactive');
+
+									// No locked links? Deactivate all links and activate this section's one.
+										if ($nav_a.filter('.active-locked').length == 0) {
+
+											$nav_a.removeClass('active');
+											$this.addClass('active');
+
+										}
+
+									// Otherwise, if this section's link is the one that's locked, unlock it.
+										else if ($this.hasClass('active-locked'))
+											$this.removeClass('active-locked');
+
+								}
+							});
+
+					});
+
+		}
+		var burger = document.getElementById("burger");
+		$window.on("resize", function() {
+			if ($(window).width() > 735) {
+				burger.style.display = "block"
+			}
+		});
+
+		window.onscroll = function() {scrollFunction()};
+
+		function scrollFunction() {
+			if ($(window).width() < 737){
+				if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+					document.getElementById("nav").style.top = "0";
+				} else {
+					document.getElementById("nav").style.top = "-50px";
+				}
+			}
+		}
+
+})(jQuery);
